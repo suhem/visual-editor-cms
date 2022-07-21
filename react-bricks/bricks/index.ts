08fadb93ba04da0c63aa8217afdd88075299b94f
Suhem <suhem.samsung@gmail.com>
2022-07-21 16:36:54 +0400

    Initial commit of visual-content-editor

--- react-bricks/bricks/index.ts
+++ react-bricks/bricks/index.ts
@@ -2,12 +2,13 @@ import { types } from 'react-bricks/frontend'
 import website from 'react-bricks-ui/website'
 import blog from 'react-bricks-ui/blog'
 import HeroUnit from './MyHeroUnit'
+import Thumbnail from './Thumbnail'
 
 const bricks: types.Brick<any>[] = [
   ...website, // React Bricks UI
   ...blog,
   HeroUnit, // Example custom brick
-  // Put here your other bricks...
+ Thumbnail
 ]
 
 export default bricks
